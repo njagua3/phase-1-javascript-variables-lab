c830b25a11ed5785659b3774bd5cb348094b8062
njagua <njagua.gma@gmail.com>
2024-06-24 15:07:08 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+const companyName = 'Scuber'
+mostProfitableNeighborhood = 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
+
+
